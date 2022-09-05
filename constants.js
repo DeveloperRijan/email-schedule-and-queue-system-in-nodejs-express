f5259a10efd4765547943621964648165a9014e7
RI Rijan <38884716+DeveloperRijan@users.noreply.github.com>
2022-09-06 01:38:25 +0600

    Update constants.js

--- constants.js
+++ constants.js
@@ -14,6 +14,43 @@ exports.CONSTANTS = {
     },
     redisDbCredentials:{
         port:6379,
-		    host:"127.0.0.1"
+	host:"127.0.0.1"
+    },
+    dateFormats:{
+        DATE:"MM/DD/YYYY",
+        DATE_TIME:"MM/DD/YYYY HH:mm",
+        DATE_TIME_AMPM:"MM/DD/YYYY hh:mm A"
+    },
+    addUserConstants:{
+        sendSMSFrequents:{
+            no_repeat:"no_repeat",
+            repeat_30:"repeat_30",
+            repeat_60:"repeat_60"
+        },
+        sendEmail:{
+            frequent:{
+                daily:"daily",
+                weekly:"weekly",
+                monthly:"monthly"
+            }
+        },
+        realTimeDataFields:{
+            data1:"data1",
+            data2:"data2",
+            data3:"data3",
+            data4:"data4",
+            data5:"data5",
+            data6:"data6",
+            data7:"data7",
+            data8:"data8",
+            data9:"data9",
+            data10:"data10",
+            data11:"data11",
+            data12:"data12",
+            data13:"data13",
+            data14:"data14",
+            data15:"data15",
+            data16:"data16"
+        }
     }
 }
